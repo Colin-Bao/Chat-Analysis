579e260e5d18fefd4ef275aab1220f0970528746
Colin <colin_bao@163.com>
2023-07-05 04:31:34 +0800

    v2.4 - Add audio - Add img

---
+++ run_user_url_info.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.path.append('/home/ubuntu/PycharmProjects/Chat-Analysis')
+import asyncio
+from DuopeiSpider.Crawler.user_url_crawler import StaticScraper
+from DuopeiSpider.Crawler.user_info_crawler import UserInfoScraper
+from DuopeiSpider.Utils.js_tools.js_script import WEBSITE_DICT
+
+
+async def main():
+    # 异步运行
+    async with StaticScraper(30, True) as scraper:
+        tasks = [scraper.run(website) for website, _ in WEBSITE_DICT.items()]
+        await asyncio.gather(*tasks)
+
+    # 异步运行
+    async with UserInfoScraper(True) as scraper:
+        tasks = [scraper.run(website) for website, _ in WEBSITE_DICT.items()]
+        await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
